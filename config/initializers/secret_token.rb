e95ef61d081cf689cdc54173925d8d18c99b3e2b
Adam Davis <davisadam10@googlemail.com>
2013-11-24 16:41:46 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Adam2::Application.config.secret_key_base = '1527f6c4206e7774975caed503613ec60a1ae1a44e6cce6c05a33b42576b0d1ba2449f1ad79cd32c84e3caf7b462a87a862e31c3660f18a26dd027ff72d58193'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Adam2::Application.config.session_store :cookie_store, key: '_adam2_session'
